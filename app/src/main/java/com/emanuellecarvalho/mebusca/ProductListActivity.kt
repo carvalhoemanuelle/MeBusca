b423959a08a7587628376cb22a51b2399b90e940
emanuellecarvalho <emanuellecarvalho@gmail.com>
2022-06-28 11:53:21 -0300

    recyclerview funcionando com dados mockados

--- app/src/main/java/com/emanuellecarvalho/mebusca/ProductListActivity.kt
+++ app/src/main/java/com/emanuellecarvalho/mebusca/ProductListActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.emanuellecarvalho.mebusca.adapter.ProductAdapter
+import com.emanuellecarvalho.mebusca.api.CategoryListResponse
+import com.emanuellecarvalho.mebusca.api.MeliApiClient
 import com.emanuellecarvalho.mebusca.databinding.ActivityProductListBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProductListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProductListBinding
-
-    //private val adapter = ProductAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +21,11 @@ class ProductListActivity : AppCompatActivity() {
         binding = ActivityProductListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.recyclerAllProducts.adapter = ProductAdapter(listaTeste())
+        binding.recyclerAllProducts.layoutManager = LinearLayoutManager(this)
+
+
+        //Esconde a barra de navegação
         supportActionBar?.hide()
 
 
@@ -30,9 +38,30 @@ class ProductListActivity : AppCompatActivity() {
 
 
         //recebendo os dados
-        val product = intent.getParcelableArrayListExtra<Product>("PRODUCTS")
+       // val product = intent.getParcelableArrayListExtra<Product>("PRODUCTS")
        // binding.textProductName.text = product.toString()
+
+
+        //conectar o Retrofit
+        //chamada assíncrona
+        val service = MeliApiClient.createCategoryService()
+        val call: Call<List<CategoryListResponse>> = service.list("bola")
+        call.enqueue(object : Callback<List<CategoryListResponse>> { //enqueue = colocar na fila
+            override fun onResponse(
+                call: Call<List<CategoryListResponse>>,
+                response: Response<List<CategoryListResponse>>
+            ) {
+                val list = response.body()
+            }
+
+            override fun onFailure(call: Call<List<CategoryListResponse>>, t: Throwable) {
+                val s = ""
+            }
+
+        })
 
     }
 
+
 }
+
--- app/src/main/java/com/emanuellecarvalho/mebusca/adapter/ProductAdapter.kt
+++ app/src/main/java/com/emanuellecarvalho/mebusca/adapter/ProductAdapter.kt
@@ -1,31 +1,39 @@
 package com.emanuellecarvalho.mebusca.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.emanuellecarvalho.mebusca.ProductViewHolder
+import com.emanuellecarvalho.mebusca.Product
 import com.emanuellecarvalho.mebusca.R
 
-class ProductAdapter(private val context: Context) : RecyclerView.Adapter<ProductViewHolder>() {
+class ProductAdapter(private val products: MutableList<Product>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
 
     //cria as visualizações da lista
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
-        val list = LayoutInflater.from(context).inflate(R.layout.row_product, parent, false)
-        val holder = ProductViewHolder(list)
-        return holder
+        val list = LayoutInflater.from(parent.context).inflate(R.layout.row_product, parent, false)
+        return ProductViewHolder(list)
     }
 
     //exibe as visualizações pro usuário
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-
-        TODO("Not yet implemented")
+        holder.bind(products[position])
 
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return products.size
     }
 
+     class ProductViewHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+         fun bind(product: Product) {
+             val txtName = itemView.findViewById<TextView>(R.id.text_product_list);
+             val txtPrice = itemView.findViewById<TextView>(R.id.text_price_product);
+             txtName.text = product.product_name
+             txtPrice.text = product.product_price.toString()
+
+         }
+    }
 }
